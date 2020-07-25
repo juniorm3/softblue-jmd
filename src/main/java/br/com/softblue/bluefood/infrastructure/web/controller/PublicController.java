224ffd19aeb4c2425740bac91bcd37fabc65152a
Mario Junior <juniorm3@msn.com>
2020-07-25 11:04:28 -0300

    11.11 (Prática): Persistindo o cliente no banco de dados

--- src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/PublicController.java
+++ src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/PublicController.java
@@ -1,21 +1,34 @@
 package br.com.softblue.bluefood.infrastructure.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.softblue.bluefood.application.ClienteService;
 import br.com.softblue.bluefood.domain.cliente.Cliente;
 
 @Controller
 @RequestMapping(path = "/public")
 public class PublicController {
 	
+	@Autowired
+	private ClienteService clienteService;
+	
 	@GetMapping("/cliente/new")
 	public String newClient(Model model) {		
-		Cliente cliente = new Cliente();
-		cliente.setNome("Mario");
-		model.addAttribute("cliente", cliente);
+		model.addAttribute("cliente", new Cliente());
+		
+		return "cliente-cadastro";
+	}
+	
+	
+	@PostMapping(path = "/cliente/save")
+	public String saveCliente(@ModelAttribute("cliente") Cliente cliente) {
+		clienteService.saveCliente(cliente);
 		
 		return "cliente-cadastro";
 	}
