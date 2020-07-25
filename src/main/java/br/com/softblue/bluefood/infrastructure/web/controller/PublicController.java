9ff5afbb4e37b7a48536ac43c5b274e914435f95
Mario Junior <juniorm3@msn.com>
2020-07-25 12:41:17 -0300

    11.15 (Prática): Validando os campos do cliente

--- src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/PublicController.java
+++ src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/PublicController.java
@@ -1,8 +1,11 @@
 package br.com.softblue.bluefood.infrastructure.web.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,23 +17,28 @@ import br.com.softblue.bluefood.domain.cliente.Cliente;
 @Controller
 @RequestMapping(path = "/public")
 public class PublicController {
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@GetMapping("/cliente/new")
-	public String newClient(Model model) {		
+	public String newClient(Model model) {
 		model.addAttribute("cliente", new Cliente());
 		ControllerHelper.setEditMode(model, false);
-		
+
 		return "cliente-cadastro";
 	}
-	
-	
+
 	@PostMapping(path = "/cliente/save")
-	public String saveCliente(@ModelAttribute("cliente") Cliente cliente) {
-		clienteService.saveCliente(cliente);
-		
+	public String saveCliente(@ModelAttribute("cliente") @Valid 
+			Cliente cliente, Errors errors, Model model) {
+
+		if (!errors.hasErrors()) {
+			clienteService.saveCliente(cliente);
+			model.addAttribute("msg", "Cliente gravado com sucesso!");
+		}
+
+		ControllerHelper.setEditMode(model, false);
 		return "cliente-cadastro";
 	}
 
