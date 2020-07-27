9399883a9d2ae3dd7f5127bfb5f67f9e271f9ded
Mario <mario.martins@sonda.com>
2020-07-27 10:55:07 -0300

    Validando telcado numérico

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,7 +1,7 @@
 function isNumberKey(evt) {
 	var charCode = (evt.which) ? evt.which : evt.keyCode;
 
-	if ((charCode >= 48 && charCode <= 57) || charCode <= 31) {
+	if ((charCode >= 48 && charCode <= 57) || charCode <= 31 || (charCode >= 96 && charCode <= 105)) {
 		return true;
 	}
 
