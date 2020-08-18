57810aa4c7cfb6b964d5f4e1d9167f526c926cbc
Mario <mario.martins@sonda.com>
2020-08-18 11:24:30 -0300

    13.18 (Prática): Buscando restaurantes - Parte 3

--- src/main/java/br/com/softblue/bluefood/application/service/RestauranteService.java
+++ src/main/java/br/com/softblue/bluefood/application/service/RestauranteService.java
@@ -1,5 +1,6 @@
 package br.com.softblue.bluefood.application.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import br.com.softblue.bluefood.domain.cliente.ClienteRepository;
 import br.com.softblue.bluefood.domain.restaurante.Restaurante;
 import br.com.softblue.bluefood.domain.restaurante.RestauranteRepository;
 import br.com.softblue.bluefood.domain.restaurante.SearchFilter;
+import br.com.softblue.bluefood.domain.restaurante.SearchFilter.SearchType;
 
 @Service
 public class RestauranteService {
@@ -66,7 +68,18 @@ public class RestauranteService {
 	}
 	
 	public List<Restaurante> search(SearchFilter filter){
-		//TODO: Considerar critérios de filtragem
-		return restauranteRepository.findAll();
+		List<Restaurante> restaurantes = new ArrayList<>();
+
+		if(filter.getSearchType() == SearchType.Texto) {
+			restaurantes = restauranteRepository.findByNomeIgnoreCaseContaining(filter.getTexto());
+			
+		} else if(filter.getSearchType() == SearchType.Categoria) {
+			restaurantes = restauranteRepository.findByCategorias_Id(filter.getCategoriaId());
+			
+		} else {
+			throw new IllegalStateException("O tipo de busca " + filter.getSearchType() + " não é suportado");
+		}
+		
+		return restaurantes;
 	}
 }
