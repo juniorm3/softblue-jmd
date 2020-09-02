9019a60f1e7b38152179d92ba94907d56880c193
Mario <mario.martins@sonda.com>
2020-09-02 16:03:38 -0300

    14.2 (Prática): Implementando o carrinho de compras - Parte 2

--- src/main/java/br/com/softblue/bluefood/domain/pedido/ItemPedido.java
+++ src/main/java/br/com/softblue/bluefood/domain/pedido/ItemPedido.java
@@ -14,12 +14,16 @@ public class ItemPedido {
 
 	@EqualsAndHashCode.Include
 	private Integer id;
-	
+
 	private ItemCardapio itemCardapio;
-	
+
 	private Integer quantidade;
-	
+
 	private String observacoes;
-	
+
 	private BigDecimal preco;
+
+	public BigDecimal getPrecoCalculado() {
+		return preco.multiply(BigDecimal.valueOf(quantidade));
+	}
 }
