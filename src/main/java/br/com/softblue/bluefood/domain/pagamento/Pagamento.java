0b34b7ae2db92d837bb7237d029b414287213c62
Mario <mario.martins@sonda.com>
2020-09-08 12:57:21 -0300

    Compartilhamento de chave primaria em relacionamnetos OntoOne

--- src/main/java/br/com/softblue/bluefood/domain/pagamento/Pagamento.java
+++ src/main/java/br/com/softblue/bluefood/domain/pagamento/Pagamento.java
@@ -7,9 +7,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -29,11 +28,11 @@ import lombok.Setter;
 public class Pagamento implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	@NotNull
 	@OneToOne
+	@MapsId
 	private Pedido pedido;
 
 	@NotNull
