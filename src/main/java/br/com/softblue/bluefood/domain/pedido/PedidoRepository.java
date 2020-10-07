bc24d40ecaf720e1731d849e42f253e24b382de9
Mario <juniorm#@msn.com>
2020-10-06 22:37:57 -0300

    14.18 (Prática): Criando o relatório de pedidos

--- src/main/java/br/com/softblue/bluefood/domain/pedido/PedidoRepository.java
+++ src/main/java/br/com/softblue/bluefood/domain/pedido/PedidoRepository.java
@@ -1,15 +1,21 @@
 package br.com.softblue.bluefood.domain.pedido;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
-	
+
 	@Query("SELECT p FROM Pedido p WHERE p.cliente.id = ?1 ORDER BY p.data DESC")
 	public List<Pedido> listPedidoByCliente(Integer clienteId);
-	
+
 	public List<Pedido> findByRestaurante_IdOrderByDataDesc(Integer restauranteId);
+
+	public Pedido findByIdAndRestaurante_Id(Integer pedidoId, Integer restauranteId);
+	
+	@Query("SELECT p FROM Pedido p WHERE p.restaurante.id = ?1 AND p.data BETWEEN ?2 AND ?3")
+	public List<Pedido> findByDateInterval(Integer restauranteId, LocalDateTime dataInicial, LocalDateTime dataFinal);
 
 }
