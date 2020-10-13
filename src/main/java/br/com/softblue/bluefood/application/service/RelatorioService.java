48f3019bb65da35608abcc1612862c65d56b6392
Mario <juniorm#@msn.com>
2020-10-12 23:37:09 -0300

    #14.19 (Prática): Criando o relatório de faturamento de itens de pedidos

--- src/main/java/br/com/softblue/bluefood/application/service/RelatorioService.java
+++ src/main/java/br/com/softblue/bluefood/application/service/RelatorioService.java
@@ -1,6 +1,9 @@
 package br.com.softblue.bluefood.application.service;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,8 @@ import org.springframework.stereotype.Service;
 
 import br.com.softblue.bluefood.domain.pedido.Pedido;
 import br.com.softblue.bluefood.domain.pedido.PedidoRepository;
+import br.com.softblue.bluefood.domain.pedido.RelatorioItemFaturamento;
+import br.com.softblue.bluefood.domain.pedido.RelatorioItemFilter;
 import br.com.softblue.bluefood.domain.pedido.RelatorioPedidoFilter;
 
 @Service
@@ -15,29 +20,67 @@ public class RelatorioService {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
-	public List<Pedido> listPedidos(Integer restauranteId, RelatorioPedidoFilter filter){
-		
+
+	public List<Pedido> listPedidos(Integer restauranteId, RelatorioPedidoFilter filter) {
+
 		Integer pedidoId = filter.getPedidoId();
-		
-		if(pedidoId != null) {
+
+		if (pedidoId != null) {
 			Pedido pedido = pedidoRepository.findByIdAndRestaurante_Id(pedidoId, restauranteId);
 			return List.of(pedido);
 		}
-		
+
 		LocalDate dataInicial = filter.getDataInicial();
 		LocalDate dataFinal = filter.getDataFinal();
-		
-		if(dataInicial == null) {
+
+		if (dataInicial == null) {
 			return List.of();
 		}
-		
-		if(dataFinal == null) {
+
+		if (dataFinal == null) {
 			dataFinal = LocalDate.now();
 		}
-		
-		return pedidoRepository.findByDateInterval(restauranteId, dataInicial.atStartOfDay(), dataFinal.atTime(23, 59, 59));
+
+		return pedidoRepository.findByDateInterval(restauranteId, dataInicial.atStartOfDay(),
+				dataFinal.atTime(23, 59, 59));
 	}
 
+	public List<RelatorioItemFaturamento> calcularFaturamentoItens(Integer restauranteId, RelatorioItemFilter filter) {
+		List<Object[]> itensObj;
+
+		Integer itemId = filter.getItemId();
+		LocalDate dataInicial = filter.getDataInicial();
+		LocalDate dataFinal = filter.getDataFinal();
+
+		if (dataInicial == null) {
+			return List.of();
+		}
+
+		if (dataFinal == null) {
+			dataFinal = LocalDate.now();
+		}
+
+		LocalDateTime dataHoraInicial = dataInicial.atStartOfDay();
+		LocalDateTime dataHorafinal = dataFinal.atTime(23, 59, 59);
+
+		if (itemId != 0) {
+
+			itensObj = pedidoRepository.findItensForFaturamento(restauranteId, itemId, dataHoraInicial,	dataHorafinal);
+		} else {
+
+			itensObj = pedidoRepository.findItensForFaturamento(restauranteId, dataHoraInicial, dataHorafinal);
+		}
+
+		List<RelatorioItemFaturamento> itens = new ArrayList<>();
+
+		for (Object[] item : itensObj) {
+			String nome = (String) item[0];
+			Long quantidade = (Long) item[1];
+			BigDecimal valor = (BigDecimal) item[2];
+			itens.add(new RelatorioItemFaturamento(nome, quantidade, valor));
+		}
+
+		return itens;
+	}
 
 }
--- src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/RestauranteController.java
+++ src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/RestauranteController.java
@@ -19,6 +19,8 @@ import br.com.softblue.bluefood.application.service.RestauranteService;
 import br.com.softblue.bluefood.application.service.ValidationException;
 import br.com.softblue.bluefood.domain.pedido.Pedido;
 import br.com.softblue.bluefood.domain.pedido.PedidoRepository;
+import br.com.softblue.bluefood.domain.pedido.RelatorioItemFaturamento;
+import br.com.softblue.bluefood.domain.pedido.RelatorioItemFilter;
 import br.com.softblue.bluefood.domain.pedido.RelatorioPedidoFilter;
 import br.com.softblue.bluefood.domain.restaurante.CategoriaRestauranteRepository;
 import br.com.softblue.bluefood.domain.restaurante.ItemCardapio;
@@ -171,5 +173,24 @@ public class RestauranteController {
 		model.addAttribute("filter", filter);
 		
 		return "restaurante-relatorio-pedidos";
+	}
+	
+	@GetMapping("/relatorio/itens")
+	public String relatorioPedidos(
+			@ModelAttribute("relatorioItemFilter") RelatorioItemFilter filter,
+			Model model) {
+		
+		Integer restauranteId = SecurityUtils.loggedRestaurante().getId();
+		
+		List<ItemCardapio> itensCardapio = itemCardapioRepository.findByRestaurante_IdOrderByNome(restauranteId);
+		model.addAttribute("itensCardapio", itensCardapio);
+		
+		List<RelatorioItemFaturamento> itensCalculados = relatorioService.calcularFaturamentoItens(restauranteId, filter);
+		model.addAttribute("itensCalculados", itensCalculados);
+		
+		
+		model.addAttribute("relatorioItemFilter", filter);
+		
+		return "restaurante-relatorio-itens";
 	}
 }
